dac41bd50071a7ae6c8bccd905a094e9d49763c3
Jason Phillips <jason.phillips@sciome.com>
2022-06-29 08:08:39 -0400

    force poly3/4 to be monotonic for constant variance

--- src/main/java/com/sciome/bmdexpress2/util/bmds/thread/PolyFitThread.java
+++ src/main/java/com/sciome/bmdexpress2/util/bmds/thread/PolyFitThread.java
@@ -141,7 +141,7 @@ public class PolyFitThread extends Thread implements IFitThread
 				{
 					boolean mono = inputParameters.isPolyMonotonic();
 					if (inputParameters.getPolyDegree() > 2)
-						mono = false;
+						mono = true;
 					// run it in both directions.
 					double[] results1 = BMDSToxicRUtils.calculateToxicR(polyModelConstant, responsesD, dosesd,
 							inputParameters.getBmrType(), inputParameters.getBmrLevel(),
