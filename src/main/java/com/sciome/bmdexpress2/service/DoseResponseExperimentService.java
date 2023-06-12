7bf97c468a1292575633b29c63fd39a20c15b37b
jason.phillips <jason.phillips@LAP-LENOVO-JP.sciome.work>
2023-06-12 09:29:11 -0400

    add service code to calculate if probe response is step func
    this code will calculate percent changes at each dose group based on
    mean at dose group.  if the percent change exceeds threshold then it
    will be considered step function

--- src/main/java/com/sciome/bmdexpress2/service/DoseResponseExperimentService.java
+++ src/main/java/com/sciome/bmdexpress2/service/DoseResponseExperimentService.java
@@ -1,15 +1,61 @@
 package com.sciome.bmdexpress2.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.sciome.bmdexpress2.mvp.model.DoseGroup;
+import com.sciome.bmdexpress2.mvp.model.DoseResponseExperiment;
+import com.sciome.bmdexpress2.mvp.model.LogTransformationEnum;
 import com.sciome.bmdexpress2.serviceInterface.IDoseResponseExperimentService;
 
 public class DoseResponseExperimentService implements IDoseResponseExperimentService
 {
 
 	@Override
-	public boolean isStepFunction()
+	public boolean isStepFunction(List<Float> responses, DoseResponseExperiment doseResponseExp,
+			double threshold)
 	{
-		// TODO Auto-generated method stub
+		List<DoseGroup> dosegroups = doseResponseExp.getDoseGroups(responses);
+
+		List<Double> dosechanges = new ArrayList<>();
+
+		double control = transform(dosegroups.get(0).getResponseMean(), doseResponseExp);
+		double last = transform(dosegroups.get(dosegroups.size() - 1).getResponseMean(), doseResponseExp);
+
+		double totalchange = Math.abs(last - control);
+
+		for (int i = 1; i < dosegroups.size(); i++)
+		{
+			double dg1 = transform(dosegroups.get(i - 1).getResponseMean(), doseResponseExp);
+			double dg2 = transform(dosegroups.get(i).getResponseMean(), doseResponseExp);
+
+			double change = Math.abs(dg2 - dg1);
+
+			dosechanges.add(change / totalchange);
+
+		}
+
+		Double max = Collections.max(dosechanges);
+		if (max >= threshold)
+			return true;
+
 		return false;
+	}
+
+	private double transform(Double responseMean, DoseResponseExperiment doseResponseExp)
+	{
+
+		if (doseResponseExp.getLogTransformation().equals(LogTransformationEnum.BASE2))
+			return Math.pow(2.0, responseMean);
+		else if (doseResponseExp.getLogTransformation().equals(LogTransformationEnum.NONE))
+			return responseMean;
+		else if (doseResponseExp.getLogTransformation().equals(LogTransformationEnum.NATURAL))
+			return Math.pow(Math.E, responseMean);
+
+		else
+			return Math.pow(10.0, responseMean);
+
 	}
 
 }
--- src/main/java/com/sciome/bmdexpress2/serviceInterface/IDoseResponseExperimentService.java
+++ src/main/java/com/sciome/bmdexpress2/serviceInterface/IDoseResponseExperimentService.java
@@ -1,8 +1,13 @@
 package com.sciome.bmdexpress2.serviceInterface;
 
+import java.util.List;
+
+import com.sciome.bmdexpress2.mvp.model.DoseResponseExperiment;
+
 public interface IDoseResponseExperimentService
 {
 
-	public boolean isStepFunction();
+	public boolean isStepFunction(List<Float> responses, DoseResponseExperiment doseResponseExperiment,
+			double threshold);
 
 }
